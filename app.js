660803d2464a676db43e971ce0c082ab66818271
Atul <atul.bhardwaj07@gmail.com>
2020-06-21 00:41:27 +0530

    Updates

--- app.js
+++ app.js
@@ -3,14 +3,6 @@ var app = express()
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose')
 
-// var fs = require('fs');
-// var path = require('path');
-
-// Connecting to the database 
-// mongoose.connect("mongodb://localhost/Photo",
-//     { useNewUrlParser: true, useUnifiedTopology: true }, err => {
-//         console.log('connected')
-//     });
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -20,60 +12,12 @@ app.set("view engine", "ejs");
 
 app.use(express.static(('public')));
 
-// var imgModel = require('./modules');
-
-// var multer = require('multer');
-
-// var storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, 'uploads')
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, file.fieldname + '-' + Date.now())
-//     }
-// });
-
-// var upload = multer({ storage: storage });
-
-// Retriving the image 
-// app.get('/detect', (req, res) => {
-//     imgModel.find({}, (err, items) => {
-//         if (err) {
-//             console.log(err);
-//         }
-//         else {
-//             res.render('app', { items: items });
-//         }
-//     });
-// });
 
 // Retriving the image 
 app.get('/', (req, res) => {
     res.render("detect")
 });
 
-
-// Uploading the image 
-// app.post('/detect', upload.single('image'), (req, res, next) => {
-
-//     var obj = {
-//         name: req.body.name,
-//         desc: req.body.desc,
-//         img: {
-//             data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
-//             contentType: 'image/png'
-//         }
-//     }
-//     imgModel.create(obj, (err, item) => {
-//         if (err) {
-//             console.log(err);
-//         }
-//         else {
-//             // item.save(); 
-//             res.redirect('/');
-//         }
-//     });
-// });
 
 
 app.listen(process.env.PORT || '3000', err => {
--- public/script.js
+++ public/script.js
@@ -24,13 +24,13 @@ video.addEventListener('play', () => {
         const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
         const resizedDetections = faceapi.resizeResults(detections, displaySize)
         var exp = resizedDetections[0].expressions
-        if (exp.angry >= exp.happy && exp.angry >= exp.neutral && exp.angry >= exp.fearful && exp.angry >= exp.disgusted && exp.angry >= exp.sad && exp.angry >= exp.surprised) document.getElementById("emotion").innerHTML = "angry"
-        else if (exp.happy >= exp.neutral && exp.happy >= exp.fearful && exp.happy >= exp.disgusted && exp.happy >= exp.sad && exp.happy >= exp.surprised) document.getElementById("emotion").innerHTML = "happy"
-        else if (exp.neutral >= exp.fearful && exp.neutral >= exp.disgusted && exp.neutral >= exp.sad && exp.neutral >= exp.surprised) document.getElementById("emotion").innerHTML = "neutral"
-        else if (exp.fearful >= exp.disgusted && exp.fearful >= exp.sad && exp.fearful >= exp.surprised) document.getElementById("emotion").innerHTML = "fearful"
-        else if (exp.disgusted >= exp.sad && exp.disgusted >= exp.surprised) document.getElementById("emotion").innerHTML = "disgusted"
-        else if (exp.sad >= exp.surprised) document.getElementById("emotion").innerHTML = "sad"
-        else document.getElementById("emotion").innerHTML = "surprised"
+        if (exp.angry >= exp.happy && exp.angry >= exp.neutral && exp.angry >= exp.fearful && exp.angry >= exp.disgusted && exp.angry >= exp.sad && exp.angry >= exp.surprised) changeIt("Angry")
+        else if (exp.happy >= exp.neutral && exp.happy >= exp.fearful && exp.happy >= exp.disgusted && exp.happy >= exp.sad && exp.happy >= exp.surprised) changeIt("Happy")
+        else if (exp.neutral >= exp.fearful && exp.neutral >= exp.disgusted && exp.neutral >= exp.sad && exp.neutral >= exp.surprised) changeIt("Neutral")
+        else if (exp.fearful >= exp.disgusted && exp.fearful >= exp.sad && exp.fearful >= exp.surprised) changeIt("Fearful")
+        else if (exp.disgusted >= exp.sad && exp.disgusted >= exp.surprised) changeIt("Disgusted")
+        else if (exp.sad >= exp.surprised) changeIt("Sad")
+        else changeIt("Surprised")
 
         // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
         // faceapi.draw.drawDetections(canvas, resizedDetections)
@@ -38,5 +38,34 @@ video.addEventListener('play', () => {
         // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
     }, 1000)
 })
+
+function changeIt(abc) {
+    if (abc == 'Happy')
+        document.getElementById("emotion").innerHTML = "HAPPY<span>&#128516;</span>";
+    else if (abc == "Angry")
+        document.getElementById("emotion").innerHTML = "ANGRY<span>&#128544;</span>";
+    else if (abc == "Neutral")
+        document.getElementById("emotion").innerHTML = "NEUTRAL<span>&#128528;</span>";
+    else if (abc == "Sad")
+        document.getElementById("emotion").innerHTML = "SAD<span>&#128577;</span>";
+    else if (abc == "Fearful")
+        document.getElementById("emotion").innerHTML = "FEARFUL<span>&#128552;</span>";
+    else if (abc == "Disgusted")
+        document.getElementById("emotion").innerHTML = "DISGUSTED<span>&#128542;</span>";
+    else if (abc == "Surprised")
+        document.getElementById("emotion").innerHTML = "SURPRISED<span>&#128558;</span>";
+
+
+    document.getElementById("changeBg").classList.remove("Sad")
+    document.getElementById("changeBg").classList.remove("Neutral")
+    document.getElementById("changeBg").classList.remove("Surprised")
+    document.getElementById("changeBg").classList.remove("Fearful")
+    document.getElementById("changeBg").classList.remove("Happy")
+    document.getElementById("changeBg").classList.remove("Angry")
+
+
+    document.getElementById("changeBg").classList.add(abc)
+
+}
 
 console.log("hello")
