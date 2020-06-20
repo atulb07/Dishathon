9f3702d1378ad73bb21fd37bdedd9c9a934dbfad
Atul <atul.bhardwaj07@gmail.com>
2020-06-20 13:00:20 +0530

    hiten updates

--- public/script.js
+++ public/script.js
@@ -24,13 +24,13 @@ video.addEventListener('play', () => {
         const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
         const resizedDetections = faceapi.resizeResults(detections, displaySize)
         var exp = resizedDetections[0].expressions
-        if (exp.angry >= exp.happy && exp.angry >= exp.neutral && exp.angry >= exp.fearful && exp.angry >= exp.disgusted && exp.angry >= exp.sad && exp.angry >= exp.surprised) document.getElementById("emotion").innerHTML = "angry"
-        else if (exp.happy >= exp.neutral && exp.happy >= exp.fearful && exp.happy >= exp.disgusted && exp.happy >= exp.sad && exp.happy >= exp.surprised) document.getElementById("emotion").innerHTML = "happy"
-        else if (exp.neutral >= exp.fearful && exp.neutral >= exp.disgusted && exp.neutral >= exp.sad && exp.neutral >= exp.surprised) document.getElementById("emotion").innerHTML = "neutral"
-        else if (exp.fearful >= exp.disgusted && exp.fearful >= exp.sad && exp.fearful >= exp.surprised) document.getElementById("emotion").innerHTML = "fearful"
-        else if (exp.disgusted >= exp.sad && exp.disgusted >= exp.surprised) document.getElementById("emotion").innerHTML = "disgusted"
-        else if (exp.sad >= exp.surprised) document.getElementById("emotion").innerHTML = "sad"
-        else document.getElementById("emotion").innerHTML = "surprised"
+        if (exp.angry >= exp.happy && exp.angry >= exp.neutral && exp.angry >= exp.fearful && exp.angry >= exp.disgusted && exp.angry >= exp.sad && exp.angry >= exp.surprised) changeIt("Angry")
+        else if (exp.happy >= exp.neutral && exp.happy >= exp.fearful && exp.happy >= exp.disgusted && exp.happy >= exp.sad && exp.happy >= exp.surprised) changeIt("Happy")
+        else if (exp.neutral >= exp.fearful && exp.neutral >= exp.disgusted && exp.neutral >= exp.sad && exp.neutral >= exp.surprised) changeIt("Neutral")
+        else if (exp.fearful >= exp.disgusted && exp.fearful >= exp.sad && exp.fearful >= exp.surprised) changeIt("Fearful")
+        else if (exp.disgusted >= exp.sad && exp.disgusted >= exp.surprised) changeIt("Disgusted")
+        else if (exp.sad >= exp.surprised) changeIt("Sad")
+        else changeIt("Surprised")
 
         // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
         // faceapi.draw.drawDetections(canvas, resizedDetections)
@@ -38,5 +38,9 @@ video.addEventListener('play', () => {
         // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
     }, 1000)
 })
+
+function changeIt(abc) {
+    document.getElementById("emotion").innerHTML = abc;
+}
 
 console.log("hello")
